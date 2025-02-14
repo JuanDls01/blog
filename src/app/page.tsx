458e071434b34fc6d000ad7614732740f8c20048
unknown <juanignaciodelossantos01@gmail.com>
2025-02-14 10:20:58 -0300

    feat: added more invo about myself in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,37 @@
 "use client";
 
+import { Badge } from "../components/ui/badge";
+
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Hello world, I'm Juani
+        Hello world, I&apos;m Juani De los Santos
       </h1>
       <p className="mb-4">
-        {`Semi Senior Software Engineer Frontend with experience building digital 
-        products with Next.js, React and TypeScript. Passionate about technology 
-        as a tool to improve user experience and generate impact. Focused on 
-        performance optimization, scalability and usability, actively participating 
-        in all stages of development, from ideation to delivery and continuous 
-        improvement.`}
+        I&apos;m a FullStack Software Engineer at Crombie.dev, where I build
+        high-performing, robust, and high-traffic web applications using
+        technologies like <Badge variant="outline">React</Badge>,{" "}
+        <Badge variant="outline">Next.js</Badge> and{" "}
+        <Badge variant="outline">TypeScript</Badge>. My constant curiosity and
+        drive for new challenges keep me at the forefront of the latest
+        technological advancements.
+      </p>
+      <p className="mb-4">
+        Throughout my career, I have worked on MVP projects for both internal
+        and external clients, as well as on an e-commerce platform for one of
+        the world&apos;s most renowned sports brands.
+      </p>
+      <p className="mb-4">
+        Beyond my work as a developer, I&apos;m an industrial engineering
+        student on the verge of graduating—a background that has honed my
+        ability to deconstruct complex problems into simpler, manageable parts.
+        While this analytical approach has been invaluable in my career, the
+        friendships I&apos;ve made along the way are what I cherish the most.
+      </p>
+      <p className="mb-4">
+        I look forward to embracing new challenges and opportunities that drive
+        innovation and foster continuous growth.
       </p>
     </section>
   );
